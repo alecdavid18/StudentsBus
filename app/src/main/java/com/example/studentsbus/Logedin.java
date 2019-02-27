87b3b75d5e25d35fb8681052b083a66471931795
alecdavid18 <alecdavid18@gmail.com>
2019-02-27 15:38:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/studentsbus/Logedin.java
@@ -0,0 +1,13 @@
+package com.example.studentsbus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logedin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logedin);
+    }
+}
